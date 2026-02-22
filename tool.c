aac2740ae37ffa850db0891716766902eea4c74e
Shubhi815 <rastogishubhi815@gmail.com>
2026-02-22 13:02:19 +0530

    initial

--- tool.c
+++ tool.c
@@ -1,7 +1,7 @@
 #include"stdio.h"
 int atoi_mine(const char *str) {
     int i=0,num=0;
-    while(str[i]!= '\n' ||str[i]!= '\r' || str[i]!= ' ' ){     
+    while(str[i]!= '\n' ||str[i]!= '\r' || str[i]!= ' ' ||str[i]!= '\0' ){     
         int temp = str[i]-'0';
         num = num*10+temp;
         i++;  
